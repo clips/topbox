f67d4ddb8318a88da921e6e77883a757a6536858
fazzeh <c.emmery@outlook.com>
2016-07-14 10:57:16 +0200

    updated doc and dirs

--- stmt.py
+++ stmt.py
@@ -1,7 +1,7 @@
 from csv import writer, reader
 from re import sub
 from subprocess import call
-from os import path, remove
+from os import path, remove, sep
 from shutil import rmtree
 from glob import glob
 from inspect import isgenerator
@@ -12,10 +12,8 @@ from inspect import isgenerator
 
 
 class STMT:
+    """Stanford Topic Modelling Toolbox Wrapper.
 
-    """
-    Stanford Topic Modelling Toolbox Wrapper
-    =====
     This is a wrapper Class around the Stanford Topic Modelling Toolbox. It
     assumes that you have your vector space in your code, and don't want to
     bother with the `csv -> scala -> java -> csv -> extract results` process.
@@ -28,7 +26,7 @@ class STMT:
     on https://cmry.github.io/2015/06/18/topbox/.
 
     Parameters
-    -----
+    ----------
     name : string
         The name that will be appended to all the saved files. If you want to
         keep the trained model, this name can be used to load it back in.
@@ -52,12 +50,12 @@ class STMT:
         done, it might be better to just leave it to True.
 
     Attributes
-    -----
+    ----------
     dir : string
         Absolute path where the storage area of the topbox is located.
 
     Examples
-    -----
+    --------
     train = [['sports football', 'this talks about football, or soccer,
                with a goal and a ball'],
              ['sports rugby', 'here we have some document where we do a scrum
@@ -95,34 +93,33 @@ class STMT:
     """
 
     def __init__(self, name, epochs=20, mem=7000, keep=True):
-        self.dir = path.dirname(path.realpath(__file__))+'/box/'
+        self.dir = path.dirname(path.realpath(__file__)) + \
+                   '{0}box{0}'.format(sep)
         self.name = name
         self.keep = keep
         self.epochs = epochs
         self.mem = mem
 
     def boot(self, mod):
-        """
-        Boot script
-        =====
+        """Boot script.
+
         Alters the directories in the .scala files for running and testing
         L-LDA (depending on the `mod`). Uses a generic call on the .jar that
         STMT resides in.
 
         Parameters
-        -----
+        ----------
         :mod: string
             Either 'test' or 'train' for swithing states.
         """
         self.scala(mod)
-        call(["java", "-Xmx"+str(self.mem)+"m", "-jar",
-              self.dir+"tmt-0.4.0.jar", self.dir+self.name+"_"+mod+".scala"])
+        call(["java", "-Xmx" + str(self.mem) + "m", "-jar", self.dir +
+              "tmt-0.4.0.jar", self.dir + self.name + "_" + mod + ".scala"])
         self.scala(mod, 1)
 
     def store(self, space, labels, vsp_type):
-        """
-        Data to csv storage
-        =====
+        """Data to csv storage.
+
         Stores a given (sub)vectorspace to the .csv format that STMT works
         with. The space should be a dict where the key is a tuple with (int,
         str), where int is the index number and str the document its topic
@@ -133,7 +130,7 @@ class STMT:
         will feed batches of the space.
 
         Parameters
-        -----
+        ----------
         space : list
             The vector space; a list with text.
 
@@ -146,19 +143,18 @@ class STMT:
         csv_file = open("%s%s_%s.csv" % (self.dir, self.name, vsp_type), 'a')
         csv_writer = writer(csv_file)
         for i, zipped in enumerate(zip(labels, space)):
-            line = [str(i+1), zipped[0], zipped[1]]
+            line = [str(i + 1), zipped[0], zipped[1]]
             csv_writer.writerow(line)
         csv_file.close()
 
     def regex(self, f, needle, rock):
-        """
-        File name replacer
-        =====
+        """File name replacer.
+
         Function is used to flip the read object file (original .scale file)
         and write replaced cotents to this newly created file.
 
         Parameters
-        -----
+        ----------
         f : string
             Contents of the original .scala file.
 
@@ -168,7 +164,7 @@ class STMT:
         rock : string
             Basically the .read() contents of the original .scala file.
         """
-        wf = f.replace('_', self.name+'_')
+        wf = f.replace('_', self.name + '_')
         f = f.replace('_', '')
         try:
             rf = open(wf, 'r')
@@ -179,48 +175,51 @@ class STMT:
         with open(wf, 'w') as wf:
             wf.write(stack)
 
-    def scala(self, step, s=False):
-        """
-        Scala code replacer
-        =====
+    def scala(self, s, r=False):
+        """Scala code replacer.
+
         Handles the .scala text replacements. In the basefiles,the replace
         targets are `modelfile` by default. This can also be used to flip
         number of the iterations.
 
         Parameters
-        -----
-        :step: train or test
-        :s:    indicates old to new replace by default
+        ----------
+        s : string
+            Has the value of either train or test in the framework.
+
+        r : boolean, optional, default False
+            Indicates old to new replace by default.
         """
         prep, std = 'maxIterations = ', ' 5'
-        orig, new = 'modelfolder', self.dir+self.name+'_'+'train'  # train
-        o_csv, n_csv = 'datafile.csv', self.dir+self.name+'_'+step+'.csv'
-        f = self.dir+'_'+step+'.scala'
-        self.regex(f, o_csv, n_csv) if not s else self.regex(f, n_csv, o_csv)
-        self.regex(f, orig, new) if not s else self.regex(f, new, orig)
-        self.regex(f, prep+std, prep+' '+str(self.epochs)) if \
-            self.epochs else self.regex(f, prep+std, prep+std)
+        orig, new = 'modelfolder', self.dir + self.name + '_' + 'train'
+        o_csv, n_csv = 'datafile.csv', self.dir + self.name + '_' + s + '.csv'
+        f = self.dir + '_' + s + '.scala'
+        self.regex(f, o_csv, n_csv) if not r else self.regex(f, n_csv, o_csv)
+        self.regex(f, orig, new) if not r else self.regex(f, new, orig)
+        self.regex(f, prep + std, prep + ' ' + str(self.epochs)) if \
+            self.epochs else self.regex(f, prep + std, prep + std)
 
     def m_incidence(self, predicted_row, label_index, gold_standard):
-        """
-        Matrix to Incidence
-        =====
+        """Matrix to Incidence.
+
         Extracts the probabilities from the .csvs, and generates an incidence
         vector based on the correct topic labels. If a value is 'NaN', it will
         be skipped (model might have crapped up somewhere). The result is a
         zipped matrix with tuple values giving (incidence, probability).
 
         Parameters
-        -----
+        ----------
         predicted_row : list
             Predicted row in the .csv file.
+
         label_index : list
             Lookup list for topics on index number.
+
         gold_standard : list
             Lookup list for correct topics per document.
 
         Return
-        -----
+        ------
         vector : list of lists
             Incidence matrix with: list(list(tuple(incidence, probability))).
         """
@@ -228,14 +227,13 @@ class STMT:
             return
         else:
             vector = [(1 if label_index[i] in gold_standard else 0,
-                       float(predicted_row[i+1])) for i in
+                       float(predicted_row[i + 1])) for i in
                       range(len(label_index))]
             return vector
 
     def get_scores(self, label_index, predicted_weights, true_labels):
-        """
-        Grab results
-        =====
+        """Grab results.
+
         Given the labelled and original file, retrieve for each
         vector: the correct label, ranks and probabilities. Get
         tuple vector, unzip it and add the incidence part to
@@ -243,11 +241,15 @@ class STMT:
         sklearn arrays for evluation).
 
         Parameters
-        -----
+        ----------
         label_index : list of tuples
             Enumerated list with topic indexes.
-        predicted_weights : csv file containing label confidences
-        true_labels : csv file containing original material
+
+        predicted_weights : string
+            Csv file directory containing label confidences.
+
+        true_labels : string
+            Csv file directory containing original material.
 
         Return
         ------
@@ -270,16 +272,15 @@ class STMT:
         return y_true, y_score
 
     def to_array(self, y_true, y_score):
-        """
-        To sklean-ready array
-        =====
+        """To sklean-ready array.
+
         Converts the incidence matrix and its probabilites to a numpy format.
         Also cleans out columns that produce a sum of zeroes; this results in
         a division by zero error when determining recall. Dependencies are
         both numpu and scipy.
 
         Parameters
-        -----
+        ----------
         y_true : list of integers
             Binary list (incidence matrix).
 
@@ -287,7 +288,7 @@ class STMT:
             Probabilities per topic.
 
         Return
-        -----
+        ------
         (y_true, y_score): numpy arrays
             Filtered and converted version of y_true and y_score input.
         """
@@ -314,9 +315,8 @@ class STMT:
         return lab_reduce(np.asarray(y_true), np.asarray(y_score))
 
     def results(self, true_labels, array=False):
-        """
-        Results grabber
-        =====
+        """Results grabber.
+
         Finds the predicted document topic distribution and label index for the
         model, then retrieves the actual labels from the original file and
         serves these to self.get_scores.
@@ -330,7 +330,7 @@ class STMT:
             the class if you do not want to work with those.
 
         Return
-        -----
+        ------
         y_true, y_score : list, list
             List of lists incidence matrix (binary) and list of lists document
             topic probabilities.
@@ -338,12 +338,13 @@ class STMT:
         DTDA = 'document-topic-distributions-res'  # doctop file
         LIDX = '00000/label-index'                 # label index
 
-        orf = open("%s%s_%s/%s.txt" % (self.dir, self.name,
-                                       'train', LIDX), 'r')
+        orf = open("%s%s_%s%s%s.txt" % (self.dir, self.name, sep,
+                                        'train', LIDX), 'r')
         label_index = orf.read().lower().split('\n')[:-1]
 
-        lbf = open("%s%s_%s/%s_%s-%s.csv" % (self.dir, self.name, 'train',
-                                             self.name, 'test', DTDA), 'r')
+        lbf = \
+            open("%s%s_%s%s%s_%s-%s.csv" % (self.dir, self.name, 'train',
+                                            self.name, sep, 'test', DTDA), 'r')
         predicted_weights = reader(lbf)
 
         y_true, y_score = self.get_scores(label_index, predicted_weights,
@@ -358,25 +359,24 @@ class STMT:
         self.cleanup(step='results')
         return y_true, y_score
 
-    def cleanup(self, all=False, step=False):
-        """
-        Cleanup module
-        =====
+    def cleanup(self, rmall=False, step=False):
+        """Cleanup module.
+
         If the user wants the trained model to be kept, it will only remove the
         .csvs and wordcounts. Otherwise, it also dumps the fully trained model
         in self.train.
 
         Parameters
-        -----
-        all : bool, optional, default False
+        ----------
+        rmall : bool, optional, default False
             Can be used to remove ALL files from box.
 
         step : bool, optional, default False
             Indicates the step so that it will keep the compressed and model
             files.
         """
-        pattern = self.name+'_*' if not all else '*_*'
-        files = glob(self.dir+pattern)
+        pattern = self.name + '_*' if not rmall else '*_*'
+        files = glob(self.dir + pattern)
         for f in files:
             if not self.keep and step != 'results':
                 rmtree(f) if '.' not in f else remove(f)
@@ -384,16 +384,15 @@ class STMT:
                 remove(f) if '.' in f and '.gz' not in f else None
 
     def run(self, space, labels, step):
-        """
-        Main runner
-        =====
+        """Main runner.
+
         Checks if the given space is given in a generator for batching, writes
         it out to a csv with self.store, then self.boot-s the model in either
         train or test mode. If it's in test, it will return the results so that
         self,results does not have to be used.
 
         Parameters
-        -----
+        ----------
         space : list
             The vector space; a list with text.
 
@@ -413,15 +412,14 @@ class STMT:
         self.cleanup()
 
     def train(self, space, labels):
-        """
-        Sugar train
-        =====
+        """Sugar train.
+
         Will train a previously untrained STMT instance on the given
         vectorspace. Please check the store function for space requirements.
         Can accept a generator for both space and labels.
 
         Parameters
-        -----
+        ----------
         space : list
             The vector space; a list with text.
 
@@ -431,15 +429,14 @@ class STMT:
         self.run(space, labels, 'train')
 
     def test(self, space, labels):
-        """
-        Sugar test
-        =====
+        """Sugar test.
+
         Will test a previously trained STMT instance on the given vectorspace.
         Please check the store function for space requirements.
         Can accept a generator for both space and labels.
 
         Parameters
-        -----
+        ----------
         space : list
             The vector space; a list with text.
 
